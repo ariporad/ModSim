e88cd5bb9db376f63cf966938e27952b986da208
Ari Porad <ari@ariporad.com>
2020-12-06 15:48:34 -0500

    feat: add animation

---
+++ animate.m
@@ -0,0 +1,30 @@
+radius_earth = 6371071; % m
+radius_orbit = 257495 + radius_earth; % m
+[timestamps, positions, velocities] = simulate_launch([0 1000000], [radius_earth + 1, 0, 0, -9500]);
+
+% Make live script support animations
+% https://www.mathworks.com/matlabcentral/answers/271470-does-the-live-editor-support-figure-animations#answer_374998
+s = settings;
+s.matlab.editor.AllowFigureAnimation.TemporaryValue = 1;
+
+
+clf; axis equal; hold on;
+
+% axis_max = max(max(positions));
+% axis_min = min(min(positions));
+% 
+% xlim([axis_min axis_max]);
+% ylim([axis_min axis_max]);
+
+viscircles([0 0], radius_earth, 'Color', 'blue');
+text(0, 0, "Earth", "HorizontalAlignment","center", "VerticalAlignment","middle", "Color", "blue", "FontSize", 30);
+
+l = animatedline();
+
+for i=1:size(positions, 1)
+    addpoints(l, positions(i, 1), positions(i, 2));
+    drawnow
+end
+
+%plot(positions(:, 1), positions(:, 2), "r");
+hold off;
