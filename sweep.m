5673e41256ae55f09a3a4d3e8ec06de5a6950d76
Ari Porad <ari@ariporad.com>
2020-12-06 20:41:44 -0500

    WIP: excentricity detection

--- sweep.m
+++ sweep.m
@@ -15,11 +15,38 @@ function res = sweep(velocity_range, timespan)
         [~, positions_cart, ~] = simulate_launch(timespan, [radius_earth + 1, 0, 0, velocity_range(i)]);
         [thetas, rhos] = cart2pol(positions_cart(:, 1), positions_cart(:, 2));
         
+        thetas = mod(thetas + (2 * pi), 2 * pi);
+        
         max_heights(i) = max(rhos);
         
         % Start at the end of theta
         % Store first angle
         % Work backward until a point is found with angle <= the first
         % angle
+   
+        first_theta = thetas(end)
+        theta_direction = sign(thetas(end - 1) - first_theta);
+        assert(theta_direction ~= 0);
+        
+        prev_theta = first_theta;
+        theta_lower_bound = length(thetas);
+        fprintf("Pre-Loop %d\n", length(thetas));
+        for j=(length(thetas) - 1):-1:1
+            sign_1 = sign(thetas(j) - prev_theta);
+            sign_2 = sign(thetas(j) - prev_theta - (theta_direction * 2 * pi));
+           if (sign_1 ~= theta_direction) && sign_1 ~= 0 && (sign_2 ~= theta_direction) && sign_2 ~= 0
+               fprintf("BREAKING! s1: %d s2: %d j: %d theta_direction: %d prev_theta: %d theta: %d\n", sign_1, sign_2, j, theta_direction, prev_theta, thetas(j)); 
+               break
+           end
+           
+           prev_theta = thetas(j);
+           theta_lower_bound = j;
+        end
+        
+        thetas_in_last_orbit = thetas(theta_lower_bound:end)
+        
+        polarplot(thetas_in_last_orbit, ones(length(thetas_in_last_orbit), 1));
+        pax = gca;
+        pax.ThetaAxisUnits = 'radians';
     end
 end
